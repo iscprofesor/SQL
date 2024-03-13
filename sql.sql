91668a90bf71b25aa38ff278ea5047daedc0626d
iscprofesor <106636766+iscprofesor@users.noreply.github.com>
2024-03-12 21:42:12 -0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -153,6 +153,15 @@ SELECT
 FROM
   `customer_data.customer_purchase`
 
+SELECT
+  customer_id,
+  CASE
+    WHEN first_name = 'Tnoy' THEN 'Tony'
+    ELSE first_name
+    END AS cleaned_name
+  FROM
+    customer_data.customer_name
+
 
 SELECT DISTINCT customer_id
 FROM
