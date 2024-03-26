9a2245d48546642c8ce35b31c4e456225379e384
iscprofesor <106636766+iscprofesor@users.noreply.github.com>
2024-03-26 12:19:47 -0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -211,3 +211,14 @@ SELECT *
 FROM Tracks
 WHERE Composer = 'Chris Cornell'
 ORDER BY GenreId DESC
+
+SELECT stn, date, IF(temp=9999.9, NULL, temp) AS temperature
+FROM bigquery-public-data.noaa_gsod.gsod2020
+
+SELECT stn, date, IF(wdsp="999.9", NULL, CAST(wdsp AS Float64)) AS wind_speed
+FROM bigquery-public-data.noaa_gsod.gsod2020
+
+SELECT stn, date, IF(prcp=99.99, 0, prcp) AS precipitation FROM `bigquery-public-data.noaa_gsod.gsod2020` WHERE stn="725030" 
+-- La Guardia
+-- OR stn="744860" -- JFK ORDER BY date DESC, stn ASC
+
