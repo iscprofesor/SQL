3bb43d28f734b2154282f88f23a9167cdf8bbb1f
iscprofesor <106636766+iscprofesor@users.noreply.github.com>
2024-04-07 19:12:36 -0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -506,3 +506,33 @@ GROUP BY
   year
 ORDER BY
   year
+
+
+SELECT
+  station_name,
+  ridership_2013,
+  ridership_2014,
+  ridership_2014 - ridership_2013 AS change_2014_raw
+FROM
+  `bigquery-public-data.new_york_subway.subway_ridership_2013_present`
+
+SELECT
+  station_name,
+  ridership_2013,
+  ridership_2014,
+  ridership_2015,
+  ridership_2016,
+  (ridership_2013 + ridership_2014 + ridership_2015 + ridership_2016) / 4 AS average
+FROM
+  `bigquery-public-data.new_york_subway.subway_ridership_2013_present`
+
+
+SELECT 
+invoice_line_id,
+invoice_id,
+unit_price,
+quantity,
+unit_price * quantity AS total_partida
+FROM
+invoice_item 
+LIMIT 5
